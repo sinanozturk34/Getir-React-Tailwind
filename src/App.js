0dc275ff437cf17377f83f9eef27a41d14554d33
Sinan Ozturk <sinansinan_@outlook.com.tr>
2024-02-18 15:32:25 +0300

    React ile Getir klon uygulama yapma

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React getir
+          Learn React 
         </a>
       </header>
     </div>
